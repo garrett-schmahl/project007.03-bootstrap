e96d3b7dd3750017752853cd84ece63090df0688
Garrett Schmahl <gschmahl@pm.me>
2021-03-26 14:03:08 -0700

    complete enough for this tye of thing

--- gitInit.bat
+++
@@ -1,6 +0,0 @@
-git init
-git add .
-git commit -m "initialize repository"
-git branch -M main
-git remote add origin https://github.com/garrett-schmahl/project007.03-bootstrap.git
-git push -u origin main
