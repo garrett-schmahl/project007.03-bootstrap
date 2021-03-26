90005b56b0dfa058838cddb6ffaf3101e48c2ef2
Garrett Schmahl <gschmahl@pm.me>
2021-03-26 13:06:07 -0700

    initialize repository

---
+++ gitInit.bat
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "initialize repository"
+git branch -M main
+git remote add origin https://github.com/garrett-schmahl/project007.03-bootstrap.git
+git push -u origin main
